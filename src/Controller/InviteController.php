9ebcb0b262d993c56bcfc56ec33d4aa775998bf6
Marijn Scholtus <marijn@healthtrain.nl>
2024-11-21 23:45:37 +0100

    Cleanup

--- src/Controller/InviteController.php
+++ src/Controller/InviteController.php
@@ -20,19 +20,19 @@ class InviteController extends AbstractController
      */
     public function invite_ht_prod(int $invitationId, int $userOrganisationConnectionId): Response
     {
-        $this->logger->info('Invite link used [HT_PROD] [InvitationId: ' . $invitationId. '] [userOrganisationConnectionId: ' . $userOrganisationConnectionId .']', array('properties' => array('type' => 'invite', 'action' => __FUNCTION__), 'invitationId' => $invitationId, 'userOrganisationConnectId', $userOrganisationConnectionId));
+        $this->logger->info('Invite link used [HT_PROD] [InvitationId: ' . $invitationId . '] [userOrganisationConnectionId: ' . $userOrganisationConnectionId . ']', array('properties' => array('type' => 'invite', 'action' => __FUNCTION__), 'invitationId' => $invitationId, 'userOrganisationConnectId', $userOrganisationConnectionId));
         return $this->redirect("https://backend.healthtrain.app/Invite/" . $invitationId . "/" . $userOrganisationConnectionId);
     }
-    
+
     public function invite_ht_at(int $invitationId, int $userOrganisationConnectionId): Response
     {
-        $this->logger->info('Invite link used [HT_AT] [InvitationId: ' . $invitationId. '] [userOrganisationConnectionId: ' . $userOrganisationConnectionId .']', array('properties' => array('type' => 'invite', 'action' => __FUNCTION__), 'invitationId' => $invitationId, 'userOrganisationConnectId', $userOrganisationConnectionId));
+        $this->logger->info('Invite link used [HT_AT] [InvitationId: ' . $invitationId . '] [userOrganisationConnectionId: ' . $userOrganisationConnectionId . ']', array('properties' => array('type' => 'invite', 'action' => __FUNCTION__), 'invitationId' => $invitationId, 'userOrganisationConnectId', $userOrganisationConnectionId));
         return $this->redirect("https://healthtrain-beheer-at.fenetre.nl/Invite/" . $invitationId . "/" . $userOrganisationConnectionId);
     }
 
     public function invite_ht_it(int $invitationId, int $userOrganisationConnectionId): Response
     {
-        $this->logger->info('Invite link used [HT_IT] [InvitationId: ' . $invitationId. '] [userOrganisationConnectionId: ' . $userOrganisationConnectionId .']', array('properties' => array('type' => 'invite', 'action' => __FUNCTION__), 'invitationId' => $invitationId, 'userOrganisationConnectId', $userOrganisationConnectionId));
+        $this->logger->info('Invite link used [HT_IT] [InvitationId: ' . $invitationId . '] [userOrganisationConnectionId: ' . $userOrganisationConnectionId . ']', array('properties' => array('type' => 'invite', 'action' => __FUNCTION__), 'invitationId' => $invitationId, 'userOrganisationConnectId', $userOrganisationConnectionId));
         return $this->redirect("https://healthtrain-beheer-it.fenetre.nl/Invite/" . $invitationId . "/" . $userOrganisationConnectionId);
     }
 
@@ -42,13 +42,13 @@ class InviteController extends AbstractController
 
     public function invite_htger_prod(int $invitationId, int $userOrganisationConnectionId): Response
     {
-        $this->logger->info('Invite link used [HTGER_PROD] [InvitationId: ' . $invitationId. '] [userOrganisationConnectionId: ' . $userOrganisationConnectionId .']', array('properties' => array('type' => 'invite', 'action' => __FUNCTION__), 'invitationId' => $invitationId, 'userOrganisationConnectId', $userOrganisationConnectionId));
+        $this->logger->info('Invite link used [HTGER_PROD] [InvitationId: ' . $invitationId . '] [userOrganisationConnectionId: ' . $userOrganisationConnectionId . ']', array('properties' => array('type' => 'invite', 'action' => __FUNCTION__), 'invitationId' => $invitationId, 'userOrganisationConnectId', $userOrganisationConnectionId));
         return $this->redirect("https://backend-germany.healthtrain.app/Invite/" . $invitationId . "/" . $userOrganisationConnectionId);
     }
 
     public function invite_htger_at(int $invitationId, int $userOrganisationConnectionId): Response
     {
-        $this->logger->info('Invite link used [HTGER_AT] [InvitationId: ' . $invitationId. '] [userOrganisationConnectionId: ' . $userOrganisationConnectionId .']', array('properties' => array('type' => 'invite', 'action' => __FUNCTION__), 'invitationId' => $invitationId, 'userOrganisationConnectId', $userOrganisationConnectionId));
+        $this->logger->info('Invite link used [HTGER_AT] [InvitationId: ' . $invitationId . '] [userOrganisationConnectionId: ' . $userOrganisationConnectionId . ']', array('properties' => array('type' => 'invite', 'action' => __FUNCTION__), 'invitationId' => $invitationId, 'userOrganisationConnectId', $userOrganisationConnectionId));
         return $this->redirect("https://healthtrain-germany-beheer-at.fenetre.nl/Invite/" . $invitationId . "/" . $userOrganisationConnectionId);
     }
 }
